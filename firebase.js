9cdf9c0b4f1706d004038b923d440604904650dc
the-bestt-coder <87234409+the-bestt-coder@users.noreply.github.com>
2021-11-25 10:14:37 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,21 @@
 import firebase from 'firebase';
 
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: "AIzaSyC3e54bxuLrqCDdjVRSO2Kqu3UGJDdapIQ",
+  authDomain: "whatsapp-clone-1e29f.firebaseapp.com",
+  projectId: "whatsapp-clone-1e29f",
+  storageBucket: "whatsapp-clone-1e29f.appspot.com",
+  messagingSenderId: "504024688209",
+  appId: "1:504024688209:web:5af9ca8025010ea13afa70"
 };
 
 const app = !firebase.apps.length
